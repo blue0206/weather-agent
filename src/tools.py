de2178cfb3fe843b3aa47b3fe30c7996893f1902
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-12-28 00:05:27 +0530

    feat: create tool to fetch weather details via api

---
+++ src/tools.py
@@ -0,0 +1,18 @@
+import requests
+
+def get_weather(city: str) -> str:
+    url = f"https://wttr.in/{city.lower()}?format=%C+%t"
+    
+    try:
+        
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            return f"The weather in {city.title()} is: {response.text}"
+        
+        return "Something went wrong while fetching the weather."
+    except Exception as e:
+        return f"Exeption occurred: {str(e)}"
+
+
+available_tools = { "get_weather": get_weather }
