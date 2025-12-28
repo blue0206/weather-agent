6128d5710e417bb550beb8fc6324a7ee701e15da
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-12-28 23:30:26 +0530

    feat: enhance `get_weather` tool with robust error handling and Pydantic validation

--- src/schemas.py
+++ src/schemas.py
@@ -1,22 +1,7 @@
 from pydantic import BaseModel, Field
-from typing import Optional
-
-
-class OutputFormat(BaseModel):
-    step: str = Field(
-        ..., description="The id of step. Example: 'PLAN', 'OUTPUT', 'TOOL' or 'START'."
-    )
-    content: Optional[str] = Field(None, description="The content of the step.")
-    tool: Optional[str] = Field(None, description="The name of tool used in the step.")
-    input: Optional[str] = Field(
-        None, description="The input to the tool used in the step."
-    )
-    output: Optional[str] = Field(
-        None, description="The output of the tool used in the step."
-    )
 
 class WeatherInput(BaseModel):
-    city: str = Field(
+    location: str = Field(
         ...,
         description="The city name for which the weather is to be retrieved."
     )
--- src/tools.py
+++ src/tools.py
@@ -1,18 +1,39 @@
 import requests
+from schemas import WeatherInput
 
-
-def get_weather(city: str) -> str:
-    url = f"https://wttr.in/{city.lower()}?format=%C+%t"
+def get_weather(input: WeatherInput) -> str:
+    location = input.location
+    url = f"https://wttr.in/{location.lower()}?format=%C+%t"
 
     try:
-        response = requests.get(url)
+        response = requests.get(url, timeout=11)
 
         if response.status_code == 200:
-            return f"The weather in {city.title()} is: {response.text}"
+            return f"The weather in {location.title()} is: {response.text}"
 
-        return "Something went wrong while fetching the weather."
+        return f"Failed to fetch weather for {location.title()}. Status Code: {response.status_code}"
     except Exception as e:
-        return f"Exeption occurred: {str(e)}"
+        return f"Exception occurred: {str(e)}"
 
 
-available_tools = {"get_weather": get_weather}
+tools_schema = [
+    {
+        "type": "function",
+        "name": "get_weather",
+        "description": "Get the current weather for a specific location.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "location": {
+                    "type": "string",
+                    "description": "The location name for which the weather is to be retrieved."
+                },
+            },
+            "required": ["location"]
+        }
+    }
+]
+
+available_functions = {
+    "get_weather": get_weather
+}
